ceb1fc50f5b41d115539d2393f3c9d63e6962891
Nazrul Islam <nnakib78@gmail.com>
2023-02-16 01:00:25 +0600

    About page done

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import styles from '@/styles/About.module.css'
 
 const About = () => {
     return (
-        <div>This is about page.</div>
+        <div className={styles.container}>
+            <h1 className={styles.center}>About Hunting Coder</h1>
+            <h2>Introduction</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores architecto obcaecati ipsam, consectetur voluptatum accusantium. Quos accusamus ut, odit id nesciunt similique, tempore libero qui, sit reprehenderit mollitia facere aspernatur magni cupiditate quibusdam? Perferendis facere eveniet ipsam id distinctio tempore enim sunt a, beatae non temporibus molestias quibusdam quaerat dolore quas provident repudiandae tenetur maxime ad odio vitae assumenda! Quis quod, nulla tempora quisquam unde corporis sed sit temporibus quaerat dolore veritatis sint suscipit alias impedit quidem perspiciatis molestias aut eos porro nemo, quibusdam a? Eveniet dolor molestiae aut, ea dolore minima illum.</p>
+            <h2>Services Offerd</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio iusto aut sed neque inventore doloremque provident non. Deleniti, praesentium? Distinctio, nesciunt doloremque, recusandae fuga eum ipsum ullam perspiciatis aperiam natus sapiente unde! Labore, molestias itaque.</p>
+            <p>We offer the following services</p>
+            <ul>
+                <li>Service 1</li>
+                <li>Service 2</li>
+                <li>Service 3</li>
+                <li>Service 4</li>
+                <li>Service 5</li>
+                <li>Service 6</li>
+                <li>Service 7</li>
+                <li>Service 8</li>
+                <li>Service 9</li>
+                <li>Service 10</li>
+            </ul>
+
+            <h2>Contact Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate est veritatis quisquam, repudiandae exercitationem voluptates corporis qui assumenda aut, eaque eligendi! Corrupti provident sed pariatur corporis voluptate. Ab repellat molestiae dolorum nemo magni velit veniam dignissimos a consequatur praesentium delectus numquam eveniet in rem soluta quas iure, voluptatibus voluptas officiis. Dolorem quas sapiente minima nulla id? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae atque cum totam eos autem perspiciatis quibusdam at praesentium ad deserunt. Et eaque molestiae rem dolores, provident officia odio optio a id aut? Voluptates, itaque veniam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni est pariatur tempore error mollitia molestiae eum ipsa, doloribus iusto delectus maxime? Rerum, esse? Veritatis, provident!</p>
+
+
+        </div>
     )
 }
 
